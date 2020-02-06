14b3b88a1c3bde9634239d7ab4c7bc53cc9c29c3
Kevin Oluoch <kevin.o.oluoch@gmail.com>
2020-02-06 16:35:18 +0300

    Remove Unneccesary comment lines

--- code/AgroDealearsTZ.R
+++ code/AgroDealearsTZ.R
@@ -1,19 +1,4 @@
 # Calculate Several Measures for Each Agrodealer Location
-  
-# 1. Travel time to nearest district HQ
-# 2. Travel time to nearest town of 50,000+
-# 3. Travel time to nearest all-weather road
-# 4. Travel time to nearest (other) agrodealer
-# 5. Number of agrodealers within 5km radius
-# 6. Number of agrodealers within 10km radius
-# 7. Number of agrodealers within 30 minutes travel time
-# 8. Number of agrodealers within 60 minutes travel time
-
-# Calculate for each district in study:
-   
-# 9. Share of rural population further than [1,2,3,4] hour of nearest agrodealer
-# 10. Share of rural population with [0,1,2,3+] agrodealers within one hour travel time
-
 
 library(osmdata)
 library(raster)
@@ -221,7 +206,6 @@ tza.trvltime.2agro.1hr <- nearestPop(1) # >1hour
 tza.trvltime.2agro.2hr <- nearestPop(2) # >2hour
 tza.trvltime.2agro.3hr <- nearestPop(3) # >3hour
 tza.trvltime.2agro.4hr <- nearestPop(4) # >4hour
-
 
 
 #-------------------------------------------------------------------------------------------------#
