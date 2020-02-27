c4933ba923d27101e0ca29cc1221db947251c8f1
Kevin Oluoch <kevin.o.oluoch@gmail.com>
2020-02-27 11:04:37 +0300

    Map agrodealers per Tanzania District

---
+++ code/MapAgrodealers.R
@@ -0,0 +1,91 @@
+# Map Agrodealers per district
+library(sf)
+library(raster)
+library(ggplot2)
+library(ggsn)
+tza.osm.roads_ <- shapefile("./data/Tanzania/Roads/OpenStreetMap/hotosm_tza_roads_lines.shp")
+wgs.prj <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+map.agrodealers.csv <- read.csv("output/tzaagrodealersoutput.csv")
+map.agrodealers.shp <- SpatialPointsDataFrame(data.frame( 
+                                                 map.agrodealers.csv$longitude, 
+                                                 map.agrodealers.csv$latitude),
+                                              data = map.agrodealers.csv,
+                                              proj4string = wgs.prj
+  )
+
+dir.create( path =  "data/shp", showWarnings = FALSE, recursive = TRUE)
+tza.international <- getData("GADM", country="TZA", level = 0, path = "data/shp")
+tza.regions <- getData("GADM", country="TZA", level = 1, path = "data/shp")
+tza.districts <- getData("GADM", country="TZA", level = 2, path = "data/shp")
+tza.wards <- getData("GADM", country="TZA", level = 3, path = "data/shp")
+
+srvy.districts <- intersect(tza.districts, map.agrodealers.shp)
+srvy.wards <- intersect(tza.wards, map.agrodealers.shp)
+
+dim(srvy.districts)
+dim(srvy.wards)
+
+
+rd.trunk <- tza.osm.roads_[which(tza.osm.roads_$highway %in% c("trunk")),]
+rd.primary <- tza.osm.roads_[which(tza.osm.roads_$highway %in% c("primary")),]
+rd.secondary <- tza.osm.roads_[which(tza.osm.roads_$highway %in% c("secondary")),]
+rd.tertiary <- tza.osm.roads_[which(tza.osm.roads_$highway %in% c("tertiary")),]
+district.names <- data.frame(District=tza.districts$NAME_2, 
+                            Longitude=coordinates(tza.districts)[,1],
+                            Latitude=coordinates(tza.districts)[,2])
+
+# Main Map
+map.all <- ggplot() +
+  geom_polygon(data = tza.wards, mapping = aes(long,lat,group=group), 
+               colour=" light grey", fill=NA,  size = 0.4, linetype = "dashed") +
+  geom_polygon(data = tza.districts, mapping = aes(long,lat,group=group), 
+               colour=" dark grey", fill=NA, size = 1, linetype = "dashed") +
+  geom_polygon(data = tza.international, mapping = aes(long,lat,group=group), 
+               colour=" black", fill=NA, size = 1.2) +
+  geom_path(data = rd.tertiary, aes(long,lat,group=group), colour="brown1", size = 0.1)+
+  geom_path(data = rd.secondary, aes(long,lat,group=group), colour="brown2", size = 0.2) +
+  geom_path(data = rd.primary, aes(long,lat,group=group), colour="brown3", size = 0.3) +
+  geom_path(data = rd.trunk, aes(long,lat,group=group), colour="brown4", size = 0.4) +
+  geom_point(data = map.agrodealers.shp@data, aes(x=longitude, y=latitude), shape=19) +
+  geom_text(data = district.names, aes(x=Longitude, y=Latitude, label=District),
+            colour = "dark grey", size = 4 )  +
+  geom_text(data = map.agrodealers.csv, aes(x=longitude, y=latitude, label=agroid),
+            colour = "dark grey", size = 4 )  +
+  coord_equal()+theme_bw()+labs(x="Longitude",y="Latitude") 
+
+dir.create("plot/agromaps/pdf", showWarnings = FALSE, recursive = TRUE)
+dir.create("plot/agromaps/png", showWarnings = FALSE, recursive = TRUE)
+
+
+pdf(file = paste0("plot/agromaps/pdf/", "StudyDistricts", ".pdf"),
+    paper = "a4",  )
+for (area_ in 1:nrow(srvy.districts)) {
+  # print(bbox(srvy.districts[area_,]))
+  area.lim <- bbox(srvy.districts[area_,])
+  min.x <- area.lim["x","min"]
+  max.x <- area.lim["x","max"]
+  min.y <- area.lim["y","min"]
+  max.y <- area.lim["y","max"]
+  area.map <- map.all + labs(title = srvy.districts[area_,]$NAME_2) +
+    coord_cartesian(xlim=c(min.x, max.x), ylim=c(min.y, max.y), default = TRUE)  +
+    geom_polygon(data = srvy.districts[area_,], mapping = aes(long,lat,group=group), 
+                 colour=" blue", fill=NA, size = 1, linetype = "dashed") +
+    scalebar(x.min=min.x, x.max=max.x, y.min=min.y, y.max=max.y, 
+             dist = ceiling((max.x - min.x)*10) + 1,
+             dist_unit = "km", transform = TRUE, model = "WGS84")
+  
+  grid::grid.newpage()
+  v1 <- grid::viewport(width = 1, height = 1, x = 0.5, y = 0.5)
+  print(area.map, vp=v1) 
+
+}
+  
+dev.off()
+
+
+plo
+
+
+
+
+
